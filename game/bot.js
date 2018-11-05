e007cf20af255ea3eb78b96086bda186258424ac
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-11-05 10:33:57 +0530

    Extracted bot to bot.js

---
+++ game/bot.js
@@ -0,0 +1,41 @@
+function Bot() {
+    Paddle.call(this);
+    this.name = "bot";
+    this.auto = true;   // set this to false to work with manual control
+    this.x = game.width - this.width - 20; // 20 margin
+    
+    var player = document.createElement("div");
+    player.style.position = "absolute";
+    player.style.width = this.width + 'px';
+    player.style.height = this.height + 'px';
+    player.style.left = this.x + 'px';
+    player.style.top = this.y + 'px';
+    player.setAttribute("id", "bot");
+    
+    this.player = player;
+    game.canvas.append(player);
+  }
+  
+  Bot.prototype = Object.create(Paddle.prototype);
+  Bot.prototype.constructor = Bot;
+  
+  Bot.prototype.draw = function () {
+    var player = this.player;
+    player.style.left = this.x +'px';
+    player.style.top = this.y +'px';
+  }
+  
+  
+  Bot.prototype.update = function () {
+    var speed = 5; // speed is set to lower than human controlled paddle
+    var self = this;
+    
+    // Bot will follow the ball (simple AI)
+    if (self.y < game.ball.y) {
+      self.yVelocity = speed;
+    } else if (self.y > game.ball.y) {
+      self.yVelocity = -speed;
+    }
+  
+    Paddle.prototype.update.apply(this, arguments);
+  }
